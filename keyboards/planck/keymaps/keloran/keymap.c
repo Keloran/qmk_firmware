a9a19d9ca4d054942e47bd86297f20264ac5cc1b
Max Hooton <keloran@develbox.info>
2021-05-01 00:01:54 +0100

    Update keymap.c

--- keyboards/planck/keymaps/keloran/keymap.c
+++ keyboards/planck/keymaps/keloran/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,    KC_Q,         KC_W,    KC_E,    KC_R,       KC_T,      KC_Y,      KC_U,       KC_I,      KC_O,     KC_P,      KC_BSPACE,
         KC_ESCAPE, KC_A,         KC_S,    KC_D,    KC_F,       KC_G,      KC_H,      KC_J,       KC_K,      KC_L,     KC_SCOLON, KC_QUOTE,
         KC_LSHIFT, KC_Z,         KC_X,    KC_C,    KC_V,       KC_B,      KC_N,      KC_M,       KC_COMMA,  KC_DOT,   KC_UP,     KC_ENTER,
-        KC_LCTRL,  TD(MISC_KEY), KC_LALT, KC_LGUI, MO(_LOWER), KC_SPACE,  KC_NO,     MO(_RAISE), KC_SLASH,  KC_LEFT,  KC_DOWN,   KC_RIGHT
+        KC_LCTRL,  TD(MISC_TAP), KC_LALT, KC_LGUI, MO(_LOWER), KC_SPACE,  KC_NO,     MO(_RAISE), KC_SLASH,  KC_LEFT,  KC_DOWN,   KC_RIGHT
   ),
 
 /* Lower
