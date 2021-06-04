06c5c105796beb0d1ee7337ecbcad52740b35fd9
Keloran <keloran@chewedfeed.com>
2021-06-04 21:35:44 +0100

    added sesame

--- keyboards/sofle/keymaps/keloran/keymap.c
+++ keyboards/sofle/keymaps/keloran/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
   KC_NO,   KC_HYPR, KC_MEH, KC_NO,      KC_F4,  KC_F5,                           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
-  KC_NO,   KC_NO,   KC_NO,  KC_NO,      KC_NO,  KC_NO,                           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+  KC_GRV,  KC_NO,   KC_NO,  KC_NO,      KC_NO,  KC_NO,                           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
   KC_TRNS, KC_EXLM, KC_AT,  KC_HASH,    KC_DLR, KC_PERC,                         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
   KC_TRNS, KC_NO,   KC_NO,  LALT(KC_3), KC_NO,  KC_NO,   KC_TRNS,       KC_TRNS, KC_NO,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
