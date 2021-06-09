44ee7559f267ddd8168b67a4c600f5cb235923d2
Keloran <keloran@chewedfeed.com>
2021-06-09 23:57:19 +0100

    better map

--- keyboards/kb_elmo/sesame/keymaps/keloran/keymap.c
+++ keyboards/kb_elmo/sesame/keymaps/keloran/keymap.c
@@ -15,21 +15,67 @@
  */
 #include QMK_KEYBOARD_H
 #include "keloran.h"
+#include "print.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT_alice(
-        KC_PAUS, KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-        KC_PSCR, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_F5,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-                 KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_LOWER),
-                 KC_LCTL,          KC_LALT,          KC_SPC,           KC_LGUI,  KC_SPC,           KC_RALT,                   KC_RCTL
+        KC_HOME, KC_GESC,   KC_1,  KC_2,    KC_3,    KC_4,    KC_5,    KC_6,         KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,     KC_BSPC,
+        KC_END, KC_TAB,    KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,         KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,    KC_BSLS,
+        KC_F5,   SUPERCAPS, KC_A,  KC_S,    KC_D,    KC_F,    KC_G,    KC_H,         KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,             KC_ENT,
+                 KC_LSFT,   KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(_LOWER), MO(_RAISE),
+                 KC_LCTL,          KC_LGUI,          KC_SPC,           TD(MISC_TAP), KC_SPC,           KC_RALT,                   KC_RCTL
     ),
     [_LOWER] = LAYOUT_alice(
-        KC_TRNS, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        KC_TRNS, KC_TRNS, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
-                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY, KC_TRNS, KC_TRNS,
-                 KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,                   KC_TRNS
+        KC_NO, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+        KC_NO, KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_NO,   KC_NO,   KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO,
+        KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_VOLD, KC_VOLU, KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY, KC_TRNS, KC_TRNS,
+        KC_NO,          KC_NO,            KC_NO,            KC_NO,   KC_NO,            KC_NO,                     KC_NO
+    ),
+    [_RAISE] = LAYOUT_alice(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,            KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS,
+        KC_NO,        KC_NO,        KC_NO,        KC_NO, KC_NO,        KC_NO,               KC_NO
+    ),
+    [_ADJUST] = LAYOUT_alice(
+        KC_NO, RESET, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO,        KC_NO,        KC_NO,        KC_NO, KC_NO,        KC_NO,               KC_NO
     )
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    //uprintf("KL: kc: 0x%04X, col: %u, row: %u\n", keycode, record->event.key.col, record->event.key.row);
+
+    if (record->event.pressed) {
+        update_wpm(keycode);
+        coding_macro(keycode);
+        switch_layer(keycode);
+        custom_funcs(keycode, record);
+    }
+
+    static uint16_t caps_timer;
+    switch (keycode) {
+        case SUPERCAPS:
+            if (record->event.pressed) {
+                caps_timer = timer_read();
+                unregister_code(KC_ESC);
+                unregister_code(KC_CAPS);
+            } else {
+                if (timer_elapsed(caps_timer) < CAPS_TIMER) {
+                    register_code(KC_ESC);
+                    unregister_code(KC_ESC);
+                } else {
+                    register_code(KC_CAPS);
+                }
+            }
+            return false;
+    }
+
+    return true;
+}
